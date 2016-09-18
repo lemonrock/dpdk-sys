a682da6cd7d352eb962ea6e7ccaeaa9d8bc71906
Raphael Cohn <raphael.cohn@stormmq.com>
2016-09-18 10:45:04 +0000

    Adjusted bindings

--- src/constants/ETH_LINK_SPEED.rs
+++ src/constants/ETH_LINK_SPEED.rs
@@ -2,19 +2,19 @@
 // Copyright © 2016 The developers of dpdk-sys. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk-sys/master/COPYRIGHT.
 
 
-pub const ETH_LINK_SPEED_100G: c_int = 16384;
-pub const ETH_LINK_SPEED_100M: c_int = 16;
-pub const ETH_LINK_SPEED_100M_HD: c_int = 8;
-pub const ETH_LINK_SPEED_10G: c_int = 256;
-pub const ETH_LINK_SPEED_10M: c_int = 4;
-pub const ETH_LINK_SPEED_10M_HD: c_int = 2;
-pub const ETH_LINK_SPEED_1G: c_int = 32;
-pub const ETH_LINK_SPEED_20G: c_int = 512;
-pub const ETH_LINK_SPEED_25G: c_int = 1024;
-pub const ETH_LINK_SPEED_2_5G: c_int = 64;
-pub const ETH_LINK_SPEED_40G: c_int = 2048;
-pub const ETH_LINK_SPEED_50G: c_int = 4096;
-pub const ETH_LINK_SPEED_56G: c_int = 8192;
-pub const ETH_LINK_SPEED_5G: c_int = 128;
-pub const ETH_LINK_SPEED_AUTONEG: c_int = 0;
-pub const ETH_LINK_SPEED_FIXED: c_int = 1;
+pub const ETH_LINK_SPEED_100G: u32 = 16384;
+pub const ETH_LINK_SPEED_100M: u32 = 16;
+pub const ETH_LINK_SPEED_100M_HD: u32 = 8;
+pub const ETH_LINK_SPEED_10G: u32 = 256;
+pub const ETH_LINK_SPEED_10M: u32 = 4;
+pub const ETH_LINK_SPEED_10M_HD: u32 = 2;
+pub const ETH_LINK_SPEED_1G: u32 = 32;
+pub const ETH_LINK_SPEED_20G: u32 = 512;
+pub const ETH_LINK_SPEED_25G: u32 = 1024;
+pub const ETH_LINK_SPEED_2_5G: u32 = 64;
+pub const ETH_LINK_SPEED_40G: u32 = 2048;
+pub const ETH_LINK_SPEED_50G: u32 = 4096;
+pub const ETH_LINK_SPEED_56G: u32 = 8192;
+pub const ETH_LINK_SPEED_5G: u32 = 128;
+pub const ETH_LINK_SPEED_AUTONEG: u32 = 0;
+pub const ETH_LINK_SPEED_FIXED: u32 = 1;
