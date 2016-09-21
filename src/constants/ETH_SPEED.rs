9b19c75662dff997c0848c930f3bd265563aefb9
Raphael Cohn <raphael.cohn@stormmq.com>
2016-09-21 20:39:56 +0000

    New bindings

--- src/constants/ETH_SPEED.rs
+++ src/constants/ETH_SPEED.rs
@@ -2,16 +2,16 @@
 // Copyright © 2016 The developers of dpdk-sys. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk-sys/master/COPYRIGHT.
 
 
-pub const ETH_SPEED_NUM_100G: c_int = 100000;
-pub const ETH_SPEED_NUM_100M: c_int = 100;
-pub const ETH_SPEED_NUM_10G: c_int = 10000;
-pub const ETH_SPEED_NUM_10M: c_int = 10;
-pub const ETH_SPEED_NUM_1G: c_int = 1000;
-pub const ETH_SPEED_NUM_20G: c_int = 20000;
-pub const ETH_SPEED_NUM_25G: c_int = 25000;
-pub const ETH_SPEED_NUM_2_5G: c_int = 2500;
-pub const ETH_SPEED_NUM_40G: c_int = 40000;
-pub const ETH_SPEED_NUM_50G: c_int = 50000;
-pub const ETH_SPEED_NUM_56G: c_int = 56000;
-pub const ETH_SPEED_NUM_5G: c_int = 5000;
-pub const ETH_SPEED_NUM_NONE: c_int = 0;
+pub const ETH_SPEED_NUM_100G: uint32_t = 100000;
+pub const ETH_SPEED_NUM_100M: uint32_t = 100;
+pub const ETH_SPEED_NUM_10G: uint32_t = 10000;
+pub const ETH_SPEED_NUM_10M: uint32_t = 10;
+pub const ETH_SPEED_NUM_1G: uint32_t = 1000;
+pub const ETH_SPEED_NUM_20G: uint32_t = 20000;
+pub const ETH_SPEED_NUM_25G: uint32_t = 25000;
+pub const ETH_SPEED_NUM_2_5G: uint32_t = 2500;
+pub const ETH_SPEED_NUM_40G: uint32_t = 40000;
+pub const ETH_SPEED_NUM_50G: uint32_t = 50000;
+pub const ETH_SPEED_NUM_56G: uint32_t = 56000;
+pub const ETH_SPEED_NUM_5G: uint32_t = 5000;
+pub const ETH_SPEED_NUM_NONE: uint32_t = 0;
