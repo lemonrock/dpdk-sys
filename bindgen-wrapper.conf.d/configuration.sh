ab843ecd1e7c6e888083f1e1aad402d8457d5bdd
Raph Cohn <raphael.cohn@stormmq.com>
2016-09-27 14:04:31 +0100

    Latest

--- bindgen-wrapper.conf.d/configuration.sh
+++ bindgen-wrapper.conf.d/configuration.sh
@@ -54,7 +54,7 @@ preprocess_before_headersFolderPath()
 		#
 		# make DESTDIR="$dpdkDestDir" prefix=/usr/local V=1 O="$dpdkBuildDir" EXTRA_CFLAGS="-D_GNU_SOURCE -I/usr/include -I$configurationFolderPath/musl-fixes -Wno-pointer-to-int-cast"
 		
-		make install T=x86_64-native-linuxapp-gcc DESTDIR="$dpdkDestDir" prefix=/usr/local V=1 O="$dpdkBuildDir" EXTRA_CFLAGS="-D_GNU_SOURCE -D_BSD_SOURCE -I/usr/include -I$configurationFolderPath/musl-fixes -Wno-pointer-to-int-cast" EXTRA_LDLIBS="-lunwind"
+		make install T=x86_64-native-linuxapp-gcc DESTDIR="$dpdkDestDir" prefix=/usr/local V=1 O="$dpdkBuildDir" EXTRA_CFLAGS="-D_GNU_SOURCE -D_BSD_SOURCE -I/usr/include -I$configurationFolderPath/musl-fixes -Wno-pointer-to-int-cast -Wl,-lunwind"
 		
 	cd - 1>/dev/null 2>/dev/null
 }
