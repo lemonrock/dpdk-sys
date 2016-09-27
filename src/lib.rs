640ba247d98b9421f7fdded7fbbcd5e11b173477
Linux User <raph@alpine.localdomain>
2016-09-27 00:49:10 +0000

    Generated lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
 // This file is part of dpdk-sys. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk-sys/master/COPYRIGHT. No part of dpdk-sys, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
 // Copyright © 2016 The developers of dpdk-sys. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk-sys/master/COPYRIGHT.
 
+
+#![allow(non_camel_case_types)]
+
+extern crate core;
+use ::core::default::Default;
+use ::core::option::Option;
+use ::core::mem::zeroed;
+use ::core::mem::transmute;
+use ::core::clone::Clone;
+use ::std::os::raw::c_char;
+use ::std::os::raw::c_uchar;
+use ::std::os::raw::c_int;
+use ::std::os::raw::c_uint;
+use ::std::os::raw::c_void;
+
+extern crate libc;
+// use self::libc::size_t;
+// use self::libc::time_t;
+// use self::libc::int64_t;
+// use self::libc::uint16_t;
+// use self::libc::uint32_t;
+// use self::libc::uint64_t;
+
